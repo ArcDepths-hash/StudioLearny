521c110da91df279a14d7ae14280f0906419c47a
ArcDepths-hash <ArcDepths@hotmail.com>
2026-06-20 22:56:27 +0200

    Implement basic Discord bot with ping command

--- bot.js
+++ bot.js
@@ -1,1 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+// Grab the token from Render's environment variables
+const token = process.env.DISCORD_TOKEN;
+
+client.once('ready', () => {
+    console.log(`Bot is online! Logged in as ${client.user.tag}`);
+});
+
+// Simple test command
+client.on('messageCreate', (message) => {
+    if (message.content === '!ping') {
+        message.channel.send('pong!');
+    }
+});
+
+client.login(token);
